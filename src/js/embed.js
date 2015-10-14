f217151242733031ac3a1cf82b83698ef92cdd92
Matt Osborn <matthew.osborn@theguardian.com>
2015-10-14 17:13:54 +0100

    embed stylings

--- src/js/embed.js
+++ src/js/embed.js
@@ -74,7 +74,8 @@ function card(country, data) {
     });
 }
 
-async function run(el) {    var spreadsheet = await reqwest({
+async function run(el) {
+    var spreadsheet = await reqwest({
         url: 'https://interactive.guim.co.uk/docsdata-test/1uYYw7D7PPph4l_KKwEGsIfSpxkaAxYrmVP_2Tjm7V_E.json',
         type: 'json', contentType: 'application/json'
     });
@@ -90,9 +91,15 @@ async function run(el) {    var spreadsheet = await reqwest({
                 'Reduction from current policies', 'Reduction from baseline year']
     keys.forEach(key => dictify(key))
 
-    el.innerHTML = Object.keys(data).map(k => card(k, data)).join('');
+    // el.innerHTML = Object.keys(data).map(k => card(k, data)).join('');
 
-    // el.innerHTML = card('Australia', data);
+    var match = /\?country=(\w+)/.exec(document.location.search);
+    if (match && data[match[1]]) {
+        el.innerHTML = card(match[1], data);
+    } else {
+        el.innerHTML = "missing country in query string. (?country=US)";
+    }
+
 
     iframeMessenger.resize()
 }
